b79065a0607c06ea5ea0de6b9f71954e9b9a0112
Abraham Choi <abraham.choi@icloud.com>
2021-05-20 01:46:38 -0700

    variables

--- main.go
+++ main.go
@@ -3,5 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	// STRINGS
+
+	var nameOne string = "Mario"
+	var nameTwo = "Luigi"
+	var nameThree string
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameOne = "Peach"
+	nameThree = "Bowser"
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameFour := "Yoshi"
+	fmt.Println(nameFour)
+
+	// INTEGERS
+
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// BITS & MEMORY
+	// int8 - Range: -128 to 127
+	// var numOne int8 = 25
+	// var numTwo int8 = -128
+	// uint8 - unsigned int, Range: 0 to 255
+	// var numThree uint16 = 256
+
+	// FLOAT
+	var scoreOne float32 = 25.98
+	var scoreTwo float64 = 7867369.3123
+	scoreThree := 1.5
+
 }
