33f9d0bd48fce6eee6522edd2e61980b5f627427
Abraham Choi <abraham.choi@icloud.com>
2021-05-21 16:21:34 -0700

    prints

--- main.go
+++ main.go
@@ -4,37 +4,20 @@ import "fmt"
 
 func main() {
 
-	// STRINGS
+	age := 34
+	name := "Abe"
+	score := 4.3
 
-	var nameOne string = "Mario"
-	var nameTwo = "Luigi"
-	var nameThree string
-	fmt.Println(nameOne, nameTwo, nameThree)
+	// Println with variable outputs
+	fmt.Println("My age is", age, "and my name is", name+".")
 
-	nameOne = "Peach"
-	nameThree = "Bowser"
-	fmt.Println(nameOne, nameTwo, nameThree)
+	// Printf (formatted strings)
+	fmt.Printf("my age is %v and my name is %v.\n", age, name)
+	fmt.Printf("my age is %q and my name is %q.\n", age, name)
+	fmt.Printf("age is of type %T\n", age)
+	fmt.Printf("you scored %0.1f points!\n", score)
 
-	nameFour := "Yoshi"
-	fmt.Println(nameFour)
-
-	// INTEGERS
-
-	var ageOne int = 20
-	var ageTwo = 30
-	ageThree := 40
-	fmt.Println(ageOne, ageTwo, ageThree)
-
-	// BITS & MEMORY
-	// int8 - Range: -128 to 127
-	// var numOne int8 = 25
-	// var numTwo int8 = -128
-	// uint8 - unsigned int, Range: 0 to 255
-	// var numThree uint16 = 256
-
-	// FLOAT
-	var scoreOne float32 = 25.98
-	var scoreTwo float64 = 7867369.3123
-	scoreThree := 1.5
-
+	// Sprintf (Save Formatted Strings)
+	var str = fmt.Sprintf("my age is %v and my name is %v.\n", age, name)
+	fmt.Println(str)
 }
